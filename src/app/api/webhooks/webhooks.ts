42a8860472bf038ea6213bcbb94a9b6e781fdd14
Blake Peterson <blake.ptrsn18@gmail.com>
2023-05-18 16:59:35 -0600

    Change logic to allow post request to go through from client app

--- src/app/api/webhooks/webhooks.ts
+++ src/app/api/webhooks/webhooks.ts
@@ -1,36 +1,24 @@
-// /api/webhooks/webhooks.ts
-
 import { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
 
-export default function handleWebhooks(req: NextApiRequest, res: NextApiResponse) {
+const handleWebhooks = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
-    // Extract relevant data from the webhook payload
-    const { event, data } = req.body as { event: string; data: any };
+    try {
+      // Make the API request to the webhook URL
+      const webhookUrl = 'https://webhooks-black.vercel.app/api/webhooks';
+      await axios.post(webhookUrl, req.body);
 
-    // Process the webhook event based on its type
-    switch (event) {
-      case 'user.created':
-        // Handle user created event
-        const userId = data.userId as string;
-        // Perform actions such as updating the database or sending notifications
-        console.log(`User created with ID: ${userId}`);
-        break;
-      case 'order.completed':
-        // Handle order completed event
-        const orderId = data.orderId as string;
-        // Perform actions such as updating the order status or sending email confirmation
-        console.log(`Order completed with ID: ${orderId}`);
-        break;
-      default:
-        // Unknown event type
-        console.log(`Unknown event: ${event}`);
-        break;
+      console.log('Webhook request sent');
+      res.status(200).json({ message: 'Webhook request sent' });
+    } catch (error) {
+      console.error('Webhook request failed');
+      console.error(error);
+      res.status(500).json({ error: 'Webhook request failed' });
     }
-
-    // Respond with a success message
-    res.status(200).json({ message: 'Webhook received and processed successfully' });
   } else {
     // Return an error for unsupported HTTP methods
     res.status(405).json({ error: 'Method Not Allowed' });
   }
-}
+};
+
+export default handleWebhooks;
