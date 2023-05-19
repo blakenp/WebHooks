89aea7bb56d2f812c84fc40628bdfd726fda10d9
Blake Peterson <blake.ptrsn18@gmail.com>
2023-05-18 18:46:01 -0600

    Corrected logic in cors.ts

--- src/app/components/cors.ts
+++ src/app/components/cors.ts
@@ -6,9 +6,16 @@ const cors = (handler: NextApiHandler): NextApiHandler => (req: NextApiRequest, 
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
+    // Respond to preflight requests immediately
     res.status(200).end();
+  } else if (req.method === 'POST') {
+    // Handle POST request logic here
+
+    console.log('Webhook request received');
+    res.status(200).json({ message: 'Webhook request received' });
   } else {
-    return handler(req, res);
+    // Return an error for unsupported HTTP methods
+    res.status(405).json({ error: 'Method Not Allowed' });
   }
 };
 
