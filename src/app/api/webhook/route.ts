a817c907aa4240461f6207905992c23a61df5c8d
Blake Peterson <blake.ptrsn18@gmail.com>
2023-05-19 15:50:41 -0600

    Revert changes

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -2,31 +2,31 @@ import { NextRequest, NextResponse } from 'next/server';
 import { middleware } from '@/app/middleware';
 import { comments } from '@/app/data/comments';
 
-interface Comment {
-    comment: string;
-}
+// interface Comment {
+//     comment: string;
+// }
 
 export async function GET(request: NextRequest, response: NextResponse) {
-    const middlewareResponse = middleware(request, response)
-    if (middlewareResponse) {
-        return middlewareResponse
-    }
+    // const middlewareResponse = middleware(request, response)
+    // if (middlewareResponse) {
+    //     return middlewareResponse
+    // }
     return NextResponse.json(comments);
 }
 
-export async function POST(request: NextRequest, response: NextResponse) {
-    const middlewareResponse = middleware(request, response)
-    if (middlewareResponse) {
-        return middlewareResponse
-    }
+// export async function POST(request: NextRequest, response: NextResponse) {
+//     const middlewareResponse = middleware(request, response)
+//     if (middlewareResponse) {
+//         return middlewareResponse
+//     }
 
-    const { comment } = await request.json()
+//     const { comment } = await request.json()
 
-    //const comment = request.body?.comment
-    const newComment = {
-        id: Date.now(),
-        text: comment
-    }
-    comments.push(newComment)
-    return NextResponse.json(newComment);
-}
+//     //const comment = request.body?.comment
+//     const newComment = {
+//         id: Date.now(),
+//         text: comment
+//     }
+//     comments.push(newComment)
+//     return NextResponse.json(newComment);
+// }
