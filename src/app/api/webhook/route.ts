2af332d22f00dc46dc9f3d931b87700e3c6a61b1
Blake Peterson <blake.ptrsn18@gmail.com>
2023-05-20 10:27:03 -0600

    Use Response object instead

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -3,37 +3,34 @@ import { middleware } from '@/app/middleware';
 import { comments } from '@/app/data/comments';
 
 export async function GET() {
-    const response = NextResponse.json(comments);
-    response.headers.set('Access-Control-Allow-Origin', '*')
-    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-    response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    response.headers.set('Access-Control-Max-Age', '86400')
-    return response
+  const response = NextResponse.json(comments);
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  response.headers.set('Access-Control-Max-Age', '86400');
+  return response;
 }
 
 export async function POST(req: NextRequest) {
-
-    const body = await req.json()
-
-    //const comment = request.body?.comment
-    const newComment = {
-        id: Date.now(),
-        text: body.comment
-    }
-    comments.push(newComment)
-    const response = NextResponse.next()
-    response.headers.set('Access-Control-Allow-Origin', '*')
-    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-    response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    response.headers.set('Access-Control-Max-Age', '86400')
-    return response
+  const body = await req.json();
+  const newComment = {
+    id: Date.now(),
+    text: body.comment
+  };
+  comments.push(newComment);
+  const response = new Response('OK');
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  response.headers.set('Access-Control-Max-Age', '86400');
+  return response;
 }
 
 export async function OPTIONS() {
-    const response = NextResponse.next();
-    response.headers.set('Access-Control-Allow-Origin', '*')
-    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-    response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    response.headers.set('Access-Control-Max-Age', '86400')
-    return response
+  const response = new Response('');
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  response.headers.set('Access-Control-Max-Age', '86400');
+  return response;
 }
