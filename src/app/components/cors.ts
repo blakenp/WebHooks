c68632a541ab1e8a33919f9a6364f4c7b99e703a
Blake Peterson <blake.ptrsn18@gmail.com>
2023-05-26 12:12:53 -0600

    Try this on for size

--- src/app/components/cors.ts
+++ src/app/components/cors.ts
@@ -1,24 +1,24 @@
 import { NextResponse } from 'next/server'
 import { headers } from 'next/headers';
 
-const allowedOrigins = ['https://api-request-two.vercel.app', 'http://localhost:3000']
+const allowedOrigins = ['https://api-request-two.vercel.app', 'http://localhost:3000'];
 
 function setCORSHeaders(response: NextResponse | Response) {
-  const requestHeaders = new Headers(response.headers)
-  const origin = requestHeaders.get('origin')
+  const requestHeaders = new Headers(response.headers);
+  const origin = allowedOrigins.find((allowedOrigin) =>
+    requestHeaders.get('Origin') === allowedOrigin
+  );
 
-  // if (origin && allowedOrigins.includes(origin)) {
-  //   response.headers.set('Access-Control-Allow-Origin', origin);
-  // }
-  
-  response.headers.set('Access-Control-Allow-Origin', 'https://api-request-two.vercel.app')
-  response.headers.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+  if (origin) {
+    response.headers.set('Access-Control-Allow-Origin', origin);
+  }
+
   response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   response.headers.set('Access-Control-Max-Age', '86400');
 }
 
-export default setCORSHeaders
+export default setCORSHeaders;
 
 
 
