09b66a408b8171192b2cfe87e5c4c857614abe24
Blake Peterson <blake.ptrsn18@gmail.com>
2023-05-26 07:37:25 -0600

    Different logic

--- src/app/components/cors.ts
+++ src/app/components/cors.ts
@@ -11,7 +11,8 @@ function setCORSHeaders(response: NextResponse | Response) {
   //   response.headers.set('Access-Control-Allow-Origin', origin);
   // }
   
-  response.headers.set('Access-Control-Allow-Origin', 'https://api-request-two.vercel.app, http://localhost:3000')
+  response.headers.set('Access-Control-Allow-Origin', 'https://api-request-two.vercel.app')
+  response.headers.set('Access-Control-Allow-Origin', 'http://localhost:3000')
   response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   response.headers.set('Access-Control-Max-Age', '86400');
