4a48684f7b1ddd67439c6f7816588d601da24f5c
Blake Peterson <blake.ptrsn18@gmail.com>
2023-05-30 11:14:34 -0600

    Maybe this syntax

--- src/app/api/webhook/[commentId]/route.ts
+++ src/app/api/webhook/[commentId]/route.ts
@@ -8,8 +8,8 @@ type Params = {
   }
 }
 
-export async function GET(req: NextRequest, { params: { commentId } }: Params) {
-  const comment = comments.find(comment => comment.id === parseInt(commentId))
+export async function GET(req: NextRequest, { params: { commentId } }: { params: { commentId: number } }) {
+  const comment = comments.find(comment => comment.id === commentId)
 
   const response = NextResponse.json(comment)
   setCORSHeaders(response, req)
